7185c5abb6dcd2b29ebea4551fcdecca36500a75
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-05-20 15:42:39 -0500

    login without all the logic

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail() //it will be make sure that is on email format
+  @IsNotEmpty() //it will make sure that the field on the request is not empty
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //it will make sure to not take extra data on the request
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
